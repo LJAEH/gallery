f743fcf9b433846ede101abf8fb4906597fa6280
ljaeh <wogurdlek33@gmail.com>
2024-03-04 17:49:17 +0900

    0304

---
+++ src/components/Card.vue
@@ -0,0 +1,48 @@
+<template>
+    <div class="card shadow-sm">
+        <img class="img-style" :src="item.imgPath" alt="">
+        <div class="card-body">
+        <p class="card-text">{{ item.name }}</p>
+        <div class="d-flex justify-content-between align-items-center">
+            <button class="btn btn-primary">구입하기</button>
+            <small class="text-muted">{{ item.price }} 원</small>
+        </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name : "Card",
+    props:{
+        item : Object
+    }
+}
+
+</script>
+
+<style scoped>
+.card .img {
+    display : inline-block;
+    width : 100%;
+    height : 250px;
+    background-size: cover;
+    background-position: center;
+}
+
+.card-body {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between; /* 위아래 여백을 동일하게 분배 */
+    height: 100%; /* 부모 요소의 높이를 100%로 설정하여 여백이 적용될 수 있도록 함 */
+}
+
+.img-style {
+    display: block;
+    width: 100%;
+    height: 250px; /* 최대 높이 설정 */
+    object-fit: cover; /* 이미지 비율 유지 */
+}
+
+
+</style>
---
+++ src/pages/Home.vue
@@ -0,0 +1,43 @@
+<template>
+    
+<main>
+
+
+<div class="album py-5 bg-light">
+  <div class="container">
+
+    <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+      
+      <div class="col" v-for="(item, idx) in state.items" :key="idx">
+        <Card :item="item"/>
+      </div>
+    </div>
+  </div>
+</div>
+</main>
+
+</template>
+
+<script>
+import { reactive } from 'vue';
+import Card from '../components/Card.vue';
+import axios from 'axios';
+
+
+export default ({
+    name: "Home",
+    components: { Card },
+    setup(){
+      const state = reactive({
+        items:[]
+      })
+      axios.get("/api/items").then(({data}) => {
+        state.items = data;
+        console.log(data);
+      })
+
+      return {state}
+    }
+}
+)
+</script>
