316374e34ff9d33ac6ac9ee0f80e9e085ad3a257
ljaeh <wogurdlek33@gmail.com>
2024-03-12 17:15:59 +0900

    0312

--- src/pages/Order.vue
+++ src/pages/Order.vue
@@ -7,92 +7,93 @@
       <div class="order-box row g-5">
         <div class="col-md-5 col-lg-4 order-md-last">
           <h4 class="d-flex justify-content-between align-items-center mb-3"><span class="text-primary">
-              주문 상품</span><span class="badge bg-primary rounded-pill">3</span></h4>
+              주문 상품</span><span class="badge bg-primary rounded-pill">{{ state.items.length }}</span></h4>
           <ul class="list-group mb-3">
-            <li class="list-group-item d-flex justify-content-between lh-sm">
+            <li class="list-group-item d-flex justify-content-between lh-sm" v-for="(i, idx) in state.items" :key="idx">
               <div>
-                <h6 class="my-0">Product name</h6><small class="text-muted">Brief description</small>
-              </div><span class="text-muted">$12</span>
+                <h6 class="my-0">{{ i.name }}</h6>
+                <small class="text-muted">Brief description</small>
+              </div>
+              <span class="text-muted">{{ numberFormatter(i.price - (i.price * i.discountPer / 100)) }}원</span>
             </li>
-            <li class="list-group-item d-flex justify-content-between lh-sm">
-              <div>
-                <h6 class="my-0">Second product</h6><small class="text-muted">Brief description</small>
-              </div><span class="text-muted">$8</span>
-            </li>
-            <li class="list-group-item d-flex justify-content-between lh-sm">
-              <div>
-                <h6 class="my-0">Third item</h6><small class="text-muted">Brief description</small>
-              </div><span class="text-muted">$5</span>
-            </li>
-            <li class="list-group-item d-flex justify-content-between bg-light">
-              <div class="text-success">
-                <h6 class="my-0">Promo code</h6><small>EXAMPLECODE</small>
-              </div><span class="text-success">−$5</span>
-            </li>
-            <li class="list-group-item d-flex justify-content-between"><span>Total (USD)</span><strong>$20</strong></li>
+
+            <li class="list-group-item d-flex justify-content-between"><span>총 금액</span><strong>{{
+                numberFormatter(computedPrice) }}원</strong></li>
           </ul>
-          <form class="card p-2">
-            <div class="input-group"><input type="text" class="form-control" placeholder="Promo code"><button
-                type="submit" class="btn btn-secondary">Redeem</button></div>
-          </form>
         </div>
         <div class="col-md-7 col-lg-8">
-          <h4 class="mb-3">주문자 정보</h4>
-          <form class="needs-validation" novalidate="">
+          <h4 class="mb-3">주문 정보</h4>
+          <div class="needs-validation" novalidate="">
             <div class="row g-3">
-            <div class="col-12"><label for="username" class="form-label">이름</label>
-              <div class="input-group has-validation"><span class="input-group-text">@</span><input type="text"
-                  class="form-control" id="username" placeholder="Username" required="">
+              <div class="col-12"><label for="username" class="form-label">이름</label>
+                <input type="text" class="form-control" id="username" placeholder="Username" v-model="state.form.name">
                 <div class="invalid-feedback"> 사용가능한 이름입니다. </div>
               </div>
+              <div class="col-12"><label for="address" class="form-label">주소</label>
+                <!-- 주소등록 api -->
+                <!-- <button class="postCode" @click="postPopup()"> 주소 등록 </button> 
+                <input type="text" v-model="postcode" placeholder="우편번호"> -->
+                <input type="text" class="form-control" v-model="state.form.address" placeholder="">
+                <div class="invalid-feedback"> 주소를 입력해주세요 </div>
+              </div>
+              <div class="col-12"><label for="address" class="form-label">상세 주소</label>
+                <input type="text" class="form-control" v-model="state.form.address" placeholder="" required="">
+                <div class="invalid-feedback"> 주소를 입력해주세요 </div>
+              </div>
             </div>
-            <div class="col-12"><label for="address" class="form-label">주소</label><input type="text"
-                class="form-control" id="address" placeholder="1234 Main St" required="">
-              <div class="invalid-feedback"> 주소를 입력해주세요 </div>
+            <hr class="my-4">
+            <h4 class="mb-3">결제수단</h4>
+            <div class="my-3">
+              <div class="form-check">
+                <input id="card" name="paymentMethod" type="radio" class="form-check-input" checked="" v-model="state.form.payment" value="card">
+                <label class="form-check-label" for="card">신용카드</label>
+              </div>
+              <div class="form-check">
+                <input id="bank" name="paymentMethod" type="radio" class="form-check-input" required="" v-model="state.form.payment" value="bank">
+                <label class="form-check-label" for="bank">무통장입금</label>
+              </div>
             </div>
-          </div>
-          <hr class="my-4">
-          <h4 class="mb-3">결제수단</h4>
-          <div class="my-3">
-            <div class="form-check"><input id="credit" name="paymentMethod" type="radio" class="form-check-input"
-                checked="" required=""><label class="form-check-label" for="credit">Credit card</label></div>
-            <div class="form-check"><input id="debit" name="paymentMethod" type="radio" class="form-check-input"
-                required=""><label class="form-check-label" for="debit">Debit card</label></div>
-            <div class="form-check"><input id="paypal" name="paymentMethod" type="radio" class="form-check-input"
-                required=""><label class="form-check-label" for="paypal">PayPal</label></div>
-          </div>
-          <div class="row gy-3">
-            <div class="col-md-6"><label for="cc-name" class="form-label">Name on card</label><input type="text"
-                class="form-control" id="cc-name" placeholder="" required=""><small class="text-muted">Full name as
-                displayed on card</small>
+            <div class="row gy-3">
+              <label for="cc-name" class="form-label">카드번호</label>
+              <input type="text" class="form-control" id="cc-name" v-model="state.form.cardNumber">
               <div class="invalid-feedback"> Name on card is required </div>
             </div>
-            <div class="col-md-6"><label for="cc-number" class="form-label">Credit card number</label><input type="text"
-                class="form-control" id="cc-number" placeholder="" required="">
-              <div class="invalid-feedback"> Credit card number is required </div>
-            </div>
+            <hr class="my-4">
+            <button class="w-100 btn btn-primary btn-lg" @click="submit()">주문하기</button>
           </div>
-          <hr class="my-4"><button class="w-100 btn btn-primary btn-lg" type="submit">주문하기</button>
-        </form>
+        </div>
       </div>
     </div>
-  </div>
 
-</div></template>
+  </div>
+</template>
 
 <script>
-import { reactive } from "vue";
+import { computed, reactive } from "vue";
 import axios from 'axios';
 
 export default {
+  data() {
+    return {
+      postcode: "",
+
+    }
+  },
   methods: {
     numberFormatter(x) {
       return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-    }
+    },
   },
   setup() {
     const state = reactive({
-      items: []
+      items: [],
+      form: {
+        name: "",
+        addess: "",
+        payment: "",
+        cardNumber: "",
+        items: ""
+      }
     })
 
     const load = () => {
@@ -102,11 +103,24 @@ export default {
       })
     }
 
+    const computedPrice = computed(() => {
+      let result = 0;
+      for (let i of state.items) {
+        result += i.price - i.price * i.discountPer / 100;
 
+      }
+      return result;
+    })
+
+    const submit = () => {
+      axios.post("/api/orders" ,state.form).then(()=>{
+        console.log('orderSuccess');
+      }) 
+    }
 
     load();
 
-    return { state }
+    return { state, computedPrice ,submit }
   }
 }
 
@@ -114,7 +128,6 @@ export default {
 
 
 <style scoped>
-
 .order-box * {
   text-align: left;
 }
@@ -122,5 +135,4 @@ export default {
 .btn {
   text-align: center;
 }
-
 </style>
